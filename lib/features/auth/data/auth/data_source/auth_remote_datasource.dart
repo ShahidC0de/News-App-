48d7b9ed5926f1d2041806c5731f3f06bfa7bf69
shahidC0de <shahidzada.cs@gmail.com>
2024-09-04 14:17:32 +0500

    removing un-necessary imports

--- lib/features/auth/data/auth/data_source/auth_remote_datasource.dart
+++ lib/features/auth/data/auth/data_source/auth_remote_datasource.dart
@@ -9,3 +9,19 @@ abstract interface class AuthRemoteDatasource {
     required String password,
   });
 }
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDatasource {
+  @override
+  Future<String> signInWithEmailAndPassword(
+      {required String email, required String password}) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailAndPassword(
+      {required String name, required String email, required String password}) {
+    // TODO: implement signUpWithEmailAndPassword
+    throw UnimplementedError();
+  }
+}
